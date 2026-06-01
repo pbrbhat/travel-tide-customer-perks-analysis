3990b08435723d11208746bd3cfc96bca2c0b20b
Pradeep Bhat <pradeep.br.bhat@gmail.com>
2026-06-01 14:04:19 +0200

    Add data understanding queries

---
+++ sql/01_data_understanding.sql
@@ -0,0 +1,37 @@
+/*
+Project: TravelTide Customer Perk Recommendation
+
+Purpose:
+Understand the structure, grain, and contents of
+the source tables before feature engineering.
+
+Tables explored:
+- users
+- sessions
+- flights
+- hotels
+*/
+
+SELECT *
+FROM users
+LIMIT 100;
+
+SELECT *
+FROM sessions
+LIMIT 100;
+
+SELECT *
+FROM flights
+LIMIT 100;
+
+SELECT *
+FROM hotels
+LIMIT 100;
+
+
+
+
+
+
+
+
